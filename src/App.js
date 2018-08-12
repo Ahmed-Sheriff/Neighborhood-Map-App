673806387ff59f3f67a4f4a5501235c7b4cb7205
ahmed sherif <ahmed.sherif.1111@gmail.com>
2018-08-12 23:18:15 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
-import './App.css';
 import scriptLoader from 'react-async-script-loader'
+import LocationList from './LocationList';
+import './App.css';
+
 
 class App extends Component {
-  state={
-    allMarkers : [
-      {title: "Nasr City", location: {lat:30.05655, lng:31.329992}, content : "Nasr City"},
-      {title: "Masr El Gdida", location: {lat:30.112315, lng:31.343851}, content : "Masr El Gdida"},
-      {title: "El Tagamoa", location: {lat:30.008487, lng: 31.428476}, content : "El Tagamoa"},
-      {title: "El ShekhZayed", location: {lat:30.049163, lng: 30.97616}, content : "El ShekhZayed"},
-      {title: "Giza", location: {lat:30.013056, lng: 31.208853}, content : "Giza"},
-      {title: "Sharm El Shekh", location: {lat:27.915817, lng: 34.329950}, content : "Sharm El Shekh"}  
-    ] ,
 
-    
+
+  state = {
+    // allMarkers : [
+    //   {title: "Nasr City", location: {lat:30.05655, lng:31.329992}, content : "Nasr City"},
+    //   {title: "Masr El Gdida", location: {lat:30.112315, lng:31.343851}, content : "Masr El Gdida"},
+    //   {title: "El Tagamoa", location: {lat:30.008487, lng: 31.428476}, content : "El Tagamoa"},
+    //   {title: "El ShekhZayed", location: {lat:30.049163, lng: 30.97616}, content : "El ShekhZayed"},
+    //   {title: "Giza", location: {lat:30.013056, lng: 31.208853}, content : "Giza"},
+    //   {title: "Sharm El Shekh", location: {lat:27.915817, lng: 34.329950}, content : "Sharm El Shekh"}  
+    // ] ,
+
+    allMarkers : []
+ 
   }
+
+  componentDidMount(){
+    fetch("https://api.foursquare.com/v2/venues/search?ll=27.2579,33.8116&client_id=NCUSJNELMTZK52IO4UXZDZTOXMDCGM1DZGO5NNP33BCZWX1Y&client_secret=K3CG2UZDUN4V2GBYLO5Z3PA0Q3GDW55R2X3LHBUHOA14R4VS&v=20180808").then(function(response){
+
+      if(response.ok){
+        debugger;
+          response.json();
+          this.setState({allMarkers : response});
+        }
+    }).catch(function(error){
+            alert('There is error with Network ' + error );
+        })
+  }
+
   componentWillReceiveProps ({ isScriptLoaded, isScriptLoadSucceed }) {
     if (isScriptLoaded && !this.props.isScriptLoaded) { // load finished
       
         let map = new window.google.maps.Map(document.getElementById('map'),{
           center : {
-                lat: 30.05655, 
-                lng: 31.329992
+                lat: 27.2579, 
+                lng: 33.8116
           },
           zoom : 8
           
@@ -29,7 +48,7 @@ class App extends Component {
         
         for(let i= 0; i < this.state.allMarkers.length; i++ ){
 
-          let marker = new window.google.maps.Marker({
+           let marker = new window.google.maps.Marker({
             position: this.state.allMarkers[i].location,
             map: map,
             title: this.state.allMarkers[i].title,
@@ -37,21 +56,19 @@ class App extends Component {
             info : this.state.allMarkers[i].content
           });
 
-          let infoWindow = new window.google.maps.InfoWindow({
-            content :  marker.info               
+           let infoWindow = new window.google.maps.InfoWindow({
+            content : `<div className = 'infoWindow'><p>${marker.info}.${marker.position}</p></div>`                
           })
           marker.addListener('click',function(){
             infoWindow.open(map,marker) ;   
           })  
         }
-          
-         
 
-             
-        
-    }
+      }
 
-  }
+  } // End Component will receive props
+
+ 
 
   render() {
 
@@ -61,7 +78,11 @@ class App extends Component {
         <div id ='map' >
             
         </div>
-      </div>
+      </div> 
+      {/* End ClassName map */}
+
+      <LocationList allMarkersList = {this.state.allMarkers} links ={this.state.links}  />
+
       </div>
     );
   }
---
+++ src/LocationList.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+
+class LocationList extends Component {
+
+    state = {
+        isToogleOn : true ,
+
+        query: '' ,
+
+        menuLinks : []
+    }
+    
+     hideMenuList = {
+     left:   "-50%"
+   }
+ 
+   displayMenuList = {
+     left:  "0"
+   }
+ 
+    toggleMenuList = () => {
+     this.setState( oldState => ({isToogleOn: oldState.isToogleOn !== true}) )
+    }
+
+    //  // Search for markers
+    //  searchMarkers (query) { 
+    //     this.setState({query: query})
+    //     this.props.allMarkersList.forEach(names =>{
+    //         names.title;
+    //         if(query === names.title) {
+    //             this.setState({menuLinks:link})
+                       
+    //           }
+    //     })
+        
+    //     } // Search for markers          
+       
+
+    render(){
+
+        return (
+
+            <div id = 'navigation'>
+            <nav className = 'heading'>
+            <header>Nieghborhood Locations</header>
+            <div className='hamburger-menu' onClick={this.toggleMenuList}  >
+               <p></p>
+               <p></p>
+               <p></p> 
+            </div>
+            </nav>
+
+            <ul className='menu-list'
+                style = {this.state.isToogleOn ? this.hideMenuList : this.displayMenuList}>
+                
+                <input  type= 'text' name = 'search' placeholder = 'Search for locations'
+                        //value = {this.state.query}
+                        // onChange={(event)=>this.searchMarkers( event.target.value)}
+                 />
+                
+                {this.props.allMarkersList.map((list,index)=> 
+                    <li className = 'link' key={index} > {list.title} </li>  
+                    
+                    )
+                }
+
+            </ul> 
+
+            </div> // End of div navigation
+
+        )
+    }
+}
+
+export default LocationList; 
